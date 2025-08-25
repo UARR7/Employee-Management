93d938a07f99a2e7cedf3af0268465e67728efdc
Akashe <capt4001@gmail.com>
2025-08-25 12:07:24 +0530

    new

--- frontend/src/components/Display.jsx
+++ frontend/src/components/Display.jsx
@@ -37,7 +37,7 @@ const Display = () => {
       employee.lastName.toLowerCase().includes(searchQuery.toLowerCase()) ||
       employee.email.toLowerCase().includes(searchQuery.toLowerCase())
   );
-
+//check
   ///super
 ///mass
   //testing
